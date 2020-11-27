8b48c91a5f408a6e2c34617e552db82f62f7332c
Javier Antón <javiantonyuste97@gmail.com>
2020-11-27 16:50:31 +0100

    Docker-compose working

--- docker-compose/spark-master/start-master.sh
+++ docker-compose/spark-master/start-master.sh
@@ -2,10 +2,6 @@
 
 export SPARK_MASTER_HOST=`hostname`
 
-. "bin/spark-config.sh"
-
-. "bin/load-spark-env.sh"
-
 mkdir -p $SPARK_MASTER_LOG
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
--- docker-compose/spark-worker/start-worker.sh
+++ docker-compose/spark-worker/start-worker.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-. "/spark/sbin/spark-config.sh"
-. "/spark/bin/load-spark-env.sh"
+# . "bin/spark-config.sh"
+# . "bin/load-spark-env.sh"
 
-mkdir -p $SPARK_WORKER_LOG
+# mkdir -p $SPARK_WORKER_LOG
+#
+# ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+#
+# /usr/bin/spark-3.0.1-bin-hadoop2.7/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
 
-export SPARK_HOME=/spark
-
-ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
-
-/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
-
-/spark/bin/spark-submit \
---master ${SPARK_MASTER_URL} \
---class ${SPARK_APPLICATION_MAIN_CLASS} \
---deploy-mode cluster \
---packages ${SPARK_SUBMIT_ARGS} \
-${SPARK_APPLICATION_JAR_LOCATION} \
+/usr/bin/spark-3.0.1-bin-hadoop2.7/bin/spark-submit \
+  --class es.upm.dit.ging.predictor.MakePrediction \
+  --master spark://spark-master:7077 \
+  --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 \
+  /flight_prediction_2.12-0.1.jar
