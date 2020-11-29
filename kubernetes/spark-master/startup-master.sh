1bf7b0e2e69f3711e93546a7722a236b692b977f
Javi Anton <javiantonyuste97@gmail.com>
2020-11-29 16:36:41 +0100

    Project working in k8s

--- kubernetes/spark-master/startup-master.sh
+++ kubernetes/spark-master/startup-master.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#export SPARK_MASTER_HOST=`hostname`
 
 /usr/bin/spark-3.0.1-bin-hadoop2.7/bin/spark-class org.apache.spark.deploy.master.Master
---
+++ kubernetes/spark-worker/start-worker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# . "bin/spark-config.sh"
+# . "bin/load-spark-env.sh"
+
+# mkdir -p $SPARK_WORKER_LOG
+#
+# ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+#
+# /usr/bin/spark-3.0.1-bin-hadoop2.7/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
+
+/usr/bin/spark-3.0.1-bin-hadoop2.7/bin/spark-submit \
+  --class es.upm.dit.ging.predictor.MakePrediction \
+  --master spark://spark-hdfs:7077 \
+  --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 \
+  /flight_prediction_2.12-0.1.jar
